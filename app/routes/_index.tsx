22748d753252446898925eb9c6ac8537f7113538
yz13-dev <yztheceo@yandex.ru>
2025-07-14 12:50:20 +0500

    update root page, add fetch to api and output in list

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,30 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 import { Button } from "@yz13/ui/button";
+import { formatRelative, parseISO } from "date-fns";
+import { ru } from "date-fns/locale";
+import { ExternalLinkIcon } from "lucide-react";
+
+export const loader = async () => {
+
+  const response = await fetch("https://api.yz13.ru/v1/news")
+
+  const articles = await response.json()
+
+  return { articles }
+}
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "App page" },
+    { title: "Новостной аггрегатор" },
     { name: "description", content: "Welcome!" },
   ];
 };
 
 export default function Index() {
+
+  const { articles } = useLoaderData<typeof loader>();
+
   return (
     <>
       <div className="max-w-lg p-8 mt-[10%] w-full mx-auto">
@@ -18,6 +34,44 @@ export default function Index() {
             Понедельник, 25 марта
           </Button>
         </div>
+      </div>
+      <div className="px-8 pb-8 max-w-lg w-full mx-auto divide-y">
+        {
+          articles.map((article: any) => {
+
+            const title = article.title
+
+            const author = article.author
+            const publishedAt = parseISO(article.published_at)
+
+            const image = article.img
+            const imageUrl = image?.url
+
+            const sourceLink = article.news_source?.url;
+            return (
+              <div className="space-y-3 py-4 gap-2 relative" key={article.id}>
+                {
+                  imageUrl &&
+                  <img src={imageUrl} className="md:w-36 w-full md:absolute relative md:-left-40 left-0 aspect-video rounded-lg object-cover" alt={title} />
+                }
+                <span className="text-lg font-medium block">{title}</span>
+                <span className="text-xs block text-muted-foreground capitalize">
+                  {formatRelative(publishedAt, new Date(), { locale: ru })}
+                </span>
+                <div className="flex items-center gap-3">
+                  {
+                    author &&
+                    <span className="text-xs text-muted-foreground">{author}</span>
+                  }
+                  <Link to={sourceLink} className="text-xs hover:underline inline-flex items-center gap-1 text-muted-foreground">
+                    Источник
+                    <ExternalLinkIcon size={12} />
+                  </Link>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </>
   );
